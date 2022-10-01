59a8def8446d906111cc21712818419d0a4e6dc7
Joschaga <josechan89_2@hotmail.com>
2022-09-30 18:31:34 -0600

    Entrega Tarea 2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2_Progra2_JoseChan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array de empleados y salario
+            String[] empleado = new[] { "Jose", "Diana", "Sofia", "Luis", "Felix" };
+            int[] salario = new[] { 4000, 2000, 900, 800, 500 };
+            var empSal = empleado.Zip(salario, (e, s) => new { empleado = e , salario = s });
+
+            //Métodos
+            foreach (var es in empSal)
+            {
+                Console.WriteLine("Nombre del empleado " + es.empleado + " - Salario de " + es.empleado + " " + es.salario + " - Aguinaldo de " + es.empleado + " " + es.salario*12);
+            }
+        }
+    }
+}
